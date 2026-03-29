f3c9f09b115f5fbc4d05cb726d78d8b34f3eba77
Jari1980 <jari.tenhunen@outlook.com>
2026-03-29 20:10:54 +0200

    generating document using given template from Open Office

--- Program.cs
+++ Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿// Current project folder (one level above bin)
+string projectRoot = Path.Combine(AppContext.BaseDirectory, "..", "..", "..");
+
+// Full path to your template
+var templatePath = Path.Combine(projectRoot, "Templates", "BaseShoppingList.ott");
+
+// Output folder in project
+var outputFolder = Path.Combine(projectRoot, "Output");
+
+var service = new WordService(templatePath, outputFolder);
+
+var replacements = new Dictionary<string, string>
+{
+    { "{{Date}}", DateTime.Now.ToShortDateString() },
+    { "{{CustomerName}}", "Broccoli Mann" },
+    { "{{Item1}}", "Apples" },
+    { "{{Qty1}}", "5" },
+    { "{{Item2}}", "Bananas" },
+    { "{{Qty2}}", "3" }
+};
+
+service.Generate(replacements, "ShoppingList.odt");
---
+++ Services/WordService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Compression;
+using System.Xml;
+
+class WordService
+{
+    private readonly string _templatePath;
+    private readonly string _outputFolder;
+
+    public WordService(string templatePath, string outputFolder)
+    {
+        _templatePath = templatePath;
+        _outputFolder = outputFolder;
+        Directory.CreateDirectory(_outputFolder);
+    }
+
+    public void Generate(Dictionary<string, string> replacements, string outputFileName)
+    {
+        string tempFolder = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(tempFolder);
+
+        ZipFile.ExtractToDirectory(_templatePath, tempFolder);
+
+        string contentXml = Path.Combine(tempFolder, "content.xml");
+        string xmlText = File.ReadAllText(contentXml);
+
+        foreach (var kv in replacements)
+        {
+            xmlText = xmlText.Replace(kv.Key, kv.Value);
+        }
+
+        File.WriteAllText(contentXml, xmlText);
+
+        string outputFile = Path.Combine(_outputFolder, outputFileName);
+        if (File.Exists(outputFile)) File.Delete(outputFile);
+        ZipFile.CreateFromDirectory(tempFolder, outputFile);
+
+        Directory.Delete(tempFolder, true);
+        Console.WriteLine("Generated: " + outputFile);
+    }
+}
